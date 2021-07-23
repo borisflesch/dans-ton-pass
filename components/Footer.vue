21739f9783eb09b1d7f1519d145467cfa984a122
Boris Flesch <boris.flesch@gmail.com>
2021-07-23 11:54:40 +0200

    footer icons

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,10 @@
 <template>
     <footer>
-      <div class="copyrights">© 2021 <a href="https://fboris.com" target="_blank">Boris Flesch</a>. Voir le <a href="https://github.com/borisflesch/dans-ton-pass" target="_blank">code source</a>.</div>
+        <div class="copyrights">
+            © 2021 <a href="https://fboris.com" target="_blank">Boris Flesch</a>
+            <br>
+            <a href="https://twitter.com/borisflesch" target="_blank"><i class="fa fa-twitter fa-lg"></i></a> - 
+            <a href="https://github.com/borisflesch/dans-ton-pass" target="_blank"><i class="fa fa-github fa-lg"></i></a>
+        </div>
     </footer>
 </template>
